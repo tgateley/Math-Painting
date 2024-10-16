29595ab71fe249b792130e075687b18a6b319cbb
Tiffany <tgateley22@gmail.com>
2024-10-15 22:14:24 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,31 @@
+class Square:
+    def __init__(self, x, y, side, color):
+        self.x = x
+        self.y = y
+        self.side = side
+        self.color = color
+
+    def draw(self, canvas):
+        pass
+
+
+class Rectangle:
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+
+    def draw(self, canvas):
+        pass
+
+
+class Canvas:
+    def __init__(self, width, height, color):
+        self.width = width
+        self.height = height
+        self.color = color
+
+    def make(self, image_path):
+        pass
