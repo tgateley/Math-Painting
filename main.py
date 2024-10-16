06a2f3e7c24b47ddc58bd25e2c21ac2a45eeadea
Tiffany <tgateley22@gmail.com>
2024-10-15 23:34:10 -0500

    Finish coding the command line interface

--- main.py
+++ main.py
@@ -7,11 +7,45 @@ answer = input("Do you want to customize the color of the canvas, Yes or No: ")
 
 match answer:
     case "Yes":
-        canvas_red =
-        canvas_green = int(input("Enter the number for Red (0-255): "))
+        canvas_red = int(input("Enter the number for Red (0-255): "))
+        canvas_green = int(input("Enter the number for Green (0-255): "))
+        canvas_blue = int(input("Enter the number for Blue (0-255): "))
+        canvas = Canvas(height=canvas_height, width=canvas_width,
+                        color=(canvas_red, canvas_green, canvas_blue))
+
+    case "No":
+        canvas = Canvas(height=canvas_height, width=canvas_width, color=(255, 255, 255))
+
+while True:
+    shape = input("Do you want to draw a rectangle or a square or quit? ")
+
+    match shape:
+        case "rectangle":
+            x = int(input('Give the X-Coordinate of the rectangle: '))
+            y = int(input("Give the Y-Coordinate of the rectangle: "))
+            rect_width = int(input("How many pixels wide is the rectangle: "))
+            rect_height = int(input("How many pixels high is the rectangle: "))
+            rect_red = int(input("Enter the number for Red (0-255): "))
+            rect_green = int(input("Enter the number for Green (0-255): "))
+            rect_blue = int(input("Enter the number for Blue (0-255): "))
+            r1 = Rectangle(x=x, y=y, height=rect_height, width=rect_width,
+                           color=(rect_red, rect_green, rect_blue))
+            r1.draw(canvas)
+
+        case "square":
+            x = int(input('Give the X-Coordinate of the square: '))
+            y = int(input("Give the Y-Coordinate of the square: "))
+            side = int(input("How many pixels is the side of the square: "))
+            square_red = int(input("Enter the number for Red (0-255): "))
+            square_green = int(input("Enter the number for Green (0-255): "))
+            square_blue = int(input("Enter the number for Blue (0-255): "))
+            s1 = Square(x=x, y=y, side=side, color=(square_red, square_green, square_blue))
+            s1.draw(canvas)
+
+        case "quit":
+            image_name = input("Give your picture a name(no spaces): ")
+            canvas.make(f'{image_name}.png')
+            break
 
 
-canvas = Canvas(height=canvas_height, width=canvas_width, color=(255, 255, 255))
-r1 = Rectangle(x=1, y=6, height=7, width=18, color=(100, 200, 125))
-r1.draw(canvas)
-canvas.make('canvas.png')
+
